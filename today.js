b76693f9aab913efcc063dd31062c35b0889a27e
luml <1520622848@qq.com>
2020-02-21 14:15:58 +0800

    remove extra folder

--- today.js
+++ today.js
@@ -1,4 +1,5 @@
 let aDay = new Date();
+// Which day
 const daysArr = [
   "Sunday",
   "Monday",
@@ -9,16 +10,24 @@ const daysArr = [
   "Saturday"
 ];
 
+// morining or afternoon
 document.querySelector("code").innerHTML = daysArr[aDay.getDay()];
-const hours = aDay.getHours();
-switch(hours < 12) {
-  case true:
+const list = aDay.toLocaleString().split(' ')
+const T = list[list.length - 1];
+
+switch(T) {
+  case "AM":
     document.querySelector("code.hours").innerHTML = "Good Morining"
     break
-  case false:
+  case "PM":
     document.querySelector("code.hours").innerHTML = "Good Afteroon"
     break
   default:
     document.querySelector("code.hours").innerHTML = "Good Day"
 }
+ 
+// which month
+const M = list[0].split("/")[0];
+document.querySelector("code.month").innerHTML = "We're in month " + M;
+
 
